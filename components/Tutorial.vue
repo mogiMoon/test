477f0ec6a162079fef9487c8abe609b757032e7f
mogiMoon <sigotomogi@gmail.com>
2024-01-19 18:44:09 +0900

    firsttime

---
+++ components/Tutorial.vue
@@ -0,0 +1,157 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div>
+        <a href="/mogi" class="head">CSS 핵심이론</a>
+      </div>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Welcome to<br />
+          your
+          <hr />
+          dadada<br />Nuxt이글 아래로
+          <hr />
+          수평선 한줄긋기 렛츠고 Application <i>qweert</i>
+        </h2>
+
+        <p>
+          감행을 정파다 지나다 방법이든 크다.<br /><br />
+          조달이니 직접 맺던 정당에 들까. <br /><b>하는 대통령에서 과학자는</b>
+          기록하던 적자부터 교수에 상당에 상식이지 사실의 역시 하다.
+          <em>"추억이나 연극까지 등 하는 사리에서 이렇다, 난 띠다" </em> 늑장에
+          함께 코치가 1명 전체를 모임으로,
+          <u>쇼크는 방식은 옮아가던 일치시키다.</u> 됨 원정은 각기 더,
+          <small>사생활을 기구는 촉구하여</small>시점도 따른 지난다.
+          <big>지난해를 개발은</big>
+          <s>"쪽 돈이, 수 자극과 혼자를 자격의 대한다 말끔하다"</s>
+          장관에서
+          <mark>임기의 기자에 조성이 이도 명실을 깃들이다. </mark>"노제의 금융과
+          태도도 있어야 현재 현행은 부동산을 이익이 발표하지만 때가 같다"
+        </p>
+        <hr />
+        <p>
+          CO<sub>2</sub> <br /><br />
+          13<sup>th</sup>
+        </p>
+        <div class="sizeup">
+          <span class="green">body</span>
+          <span
+            >{
+            <p></p>
+            }</span
+          >
+          <span class="pink"> <sup>color:</sup> </span>
+          <span class="navy"> <sup> navy; </sup> </span>
+        </div>
+        <div>
+          <ol>
+            <li>방콕 쇼핑 리스트</li>
+            <li>방콕에서 반드시 들려야 할 베스트</li>
+            <li>방콕 맛집 리스트</li>
+            <li>방콕 관광 리스트</li>
+          </ol>
+          <a href="https://www.naver.com"> Naver 바로이동</a><br />
+          <a target="_blank" href="https://www.naver.com">naver 새창 이동</a
+          ><br />
+          <a href="http://www.naver.com">
+            <img
+              src="/Users/mogi/Desktop/study/CSS-yarn/my-nuxt3-project/imgfile/naverimg.png"
+              alt="NAVER"
+            />
+          </a>
+        </div>
+        <p class="mt-3 text-gray-600">
+          We recommend you take a look at the
+          <a
+            href="https://nuxtjs.org"
+            target="_blank"
+            class="button--doc text-green-500 hover:underline"
+            >Nuxt Documentation</a
+          >, whether you are new or have previous experience with the
+          framework.<br />
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          To get started, remove
+          <code class="bg-gray-100 text-sm p-1 rounded border"
+            >components/Tutorial.vue</code
+          >
+          and start coding in
+          <code class="bg-gray-100 text-sm p-1 rounded border"
+            >pages/index.vue</code
+          >. Have fun!
+        </p>
+      </div>
+
+      <div class="flex justify-center pt-4 space-x-2">
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank"
+          ><svg
+            class="w-6 h-6 text-gray-600 hover:text-gray-800 button--github"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 2.247a10 10 0 0 0-3.162 19.487c.5.088.687-.212.687-.475c0-.237-.012-1.025-.012-1.862c-2.513.462-3.163-.613-3.363-1.175a3.636 3.636 0 0 0-1.025-1.413c-.35-.187-.85-.65-.013-.662a2.001 2.001 0 0 1 1.538 1.025a2.137 2.137 0 0 0 2.912.825a2.104 2.104 0 0 1 .638-1.338c-2.225-.25-4.55-1.112-4.55-4.937a3.892 3.892 0 0 1 1.025-2.688a3.594 3.594 0 0 1 .1-2.65s.837-.262 2.75 1.025a9.427 9.427 0 0 1 5 0c1.912-1.3 2.75-1.025 2.75-1.025a3.593 3.593 0 0 1 .1 2.65a3.869 3.869 0 0 1 1.025 2.688c0 3.837-2.338 4.687-4.563 4.937a2.368 2.368 0 0 1 .675 1.85c0 1.338-.012 2.413-.012 2.75c0 .263.187.575.687.475A10.005 10.005 0 0 0 12 2.247z"
+              fill="currentColor"
+            /></svg
+        ></a>
+        <a href="https://twitter.com/nuxt_js" target="_blank"
+          ><svg
+            class="w-6 h-6 text-gray-600 hover:text-gray-800"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23z"
+              fill="currentColor"
+            /></svg
+        ></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
+
+<style>
+.head {
+  font-size: 50px;
+  text-align: center;
+  color: aqua;
+  background: pink;
+}
+.green {
+  color: greenyellow;
+}
+.pink {
+  color: palevioletred;
+}
+.navy {
+  color: aqua;
+}
+.sizeup {
+  font-size: 45px;
+}
+</style>

1777d291ba286bc3b90fcf277d4a9dee205bd48e
mogiMoon <sigotomogi@gmail.com>
2024-01-30 10:46:17 +0900

    20240130start

---
+++ pages/clonsite.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="aaa12">hi</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+import "../css_style/clonstyle.css";
+</script>
--- pages/next.vue
+++ pages/next.vue
@@ -1,9 +1,57 @@
 <template>
   <div>
-    <div class="backpage">
-      <a href="/mogi">back to last page</a>
+    <div class="buttontop">
+      <div class="backpage">
+        <a href="/mogi">back to last page</a>
+      </div>
+      <div class="homepage">
+        <a href="/">home page</a>
+      </div>
     </div>
     <div class="nextpglogo">follow up</div>
+    <div class="boxtype">
+      <div class="floattop">float: none</div>
+      <div class="floatleft">float: left</div>
+      <div class="floatright">float: right</div>
+      <div class="clear">clear: both</div>
+    </div>
+    <div class="floattop1">float: none</div>
+    <div class="boxflex">
+      <div class="floatleft1">float: left</div>
+      <div class="floatright1">float: right</div>
+    </div>
+    <div class="clear1">clear: both</div>
+    <div class="floattop2">
+      float: none
+      <div class="boxflex2">
+        <div class="floatleft2">float: left</div>
+        <div class="floatright2">float: right</div>
+      </div>
+      clear: both
+    </div>
+    <section class="section1">
+      <article>article</article>
+      <article>article</article>
+      <article>article</article>
+    </section>
+    <div class="container">
+      <header>
+        header
+        <article class="loko">article</article>
+        <article class="navi">article</article>
+      </header>
+      <section class="section21">section</section>
+      <section class="section22">
+        <article class="sub1">article</article>
+        <article class="sub2">article</article>
+        <article class="sub3">article</article>
+      </section>
+      <footer>footer</footer>
+    </div>
+    <div class="positionp">
+      <div class="positions"></div>
+    </div>
+    <a href="/clonsite" class="head">clon Page</a>
   </div>
 </template>
 
